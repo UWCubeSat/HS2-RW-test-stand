cda643aa239af4b374ad4e9ee55c8d941aa98465
Charles Nguyen <charlesnguyentheguy@gmail.com>
2023-07-13 12:33:22 -0700

    Data collection

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ double targetPos = 0;
 #define PWM_BOUNDS 200 // range is PWM_STATIONARY +/- PWM_BOUNDS
 #define PWM_DEADZONE 0.132 // needs to be at least this percent of PWM_BOUNDS=200 to do have motor spin
 #define PWM_STATIONARY 1500
-#define REVERSED 1
+#define REVERSED 0
 Servo ESC;
 double motorSpeed = 0;
 // ------------------
@@ -44,8 +44,11 @@ const double vkD = 0.0013;
 // const double vkD = 0.00; 
 const double vkI = 0.0;
 PIDController pidSpeed(vkP, vkI, vkD);
-// TODO: consider ROV simulation for PID. Cross product the displacement vector for each target with basis vector
-//        note: Is the basis the change of basis from standard to current?
+// ------------------
+
+// -------SD---------
+#include "SD.h"
+const int chip_select = 10;
 // ------------------
 
 // FSM variables
@@ -108,6 +111,28 @@ void printMoreThings() {
     Serial.println(F(" "));
 }
 
+void printDirectory(File dir, int num_tabs) {
+  while (true) {
+    File entry =  dir.openNextFile();
+    if (!entry) {
+      break;
+    }
+    for (uint8_t i = 0; i < num_tabs; i++) {
+      Serial.print('\t');
+    }
+    Serial.print(entry.name());
+    if (entry.isDirectory()) {
+      Serial.println("/");
+      printDirectory(entry, num_tabs + 1);
+    } else {
+      // files have sizes, directories do not
+      Serial.print("\t\t");
+      Serial.println(entry.size(), DEC);
+    }
+    entry.close();
+  }
+}
+
 void setup() {
   Serial.begin(9600);
   
@@ -124,6 +149,42 @@ void setup() {
 
   updateYawAngle();
   targetPos = yawAngle;
+
+  pinMode(SS, OUTPUT);
+  if (!SD.begin(chip_select)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  }
+  Serial.println("card initialized.");
+  
+  // if (SD.exists("test.txt")) {
+  //   SD.remove("test.txt");
+  // }
+
+  File root = SD.open("/");
+  printDirectory(root, 0);
+
+  // file = SD.open("test.txt", FILE_WRITE);
+  // if (file) {
+  //   file.println("hi");
+  //   file.close();
+  // } else {
+  //   Serial.println("card failed write");
+  // }
+  // file = SD.open("test.txt", FILE_READ);
+  // if (file) {
+  //   Serial.println("test.txt:");
+
+  //   // read from the file until there's nothing else in it:
+  //   while (file.available()) {
+  //     Serial.write(file.read());
+  //   }
+  //   // close the file:
+  //   file.close();
+  // } else {
+  //   Serial.println("card failed read");
+  // }
 
   Serial.println("Setup finished!");
 
@@ -148,35 +209,37 @@ void loop() {
     updateYawSpeed();
     updateRollingAvg();
 
-    // FSM transition
-    if (controllerState == 1 && fabs(rollingAvg) > 360 /* °/s */) { // this should be the saturated deg/sec
-      controllerState = 0;
-    } else if (controllerState == 0 && fabs(rollingAvg) < 45 /* °/s */) {
-      controllerState = 1;
-    }
-    controllerState = 1;
-    
-    // FSM action 
-    float update = 0;
-    if (controllerState == 0) { // state 0 = detumble + update time for angle 
-      // update = pidSpeed.compute(0, rollingAvg, timeCur - timePrev); 
-      update = pidSpeed.compute(0, rollingAvg, timeCur - timePrev); 
-      pidAngle.compute(targetPos, yawAngle, timeCur - timePrev); // need because time is updated each iter
-    } else { // state 1 = set setpoint to calculated motor output. Error = desired pwm -avg pwm 
-      float anglePwmOut = pidAngle.compute(targetPos, yawAngle, timeCur - timePrev);
-      update = pidSpeed.compute(anglePwmOut, rollingAvg, timeCur - timePrev);
-      #if TEST
-        Serial.print("anglePwmOut: "); Serial.print(anglePwmOut);
-      #endif
-    } 
-    if (fabs(pidAngle.getError()) <= MOE) update = 0; // if withing MOE, keep constant velocity
-    if (fabs(pidSpeed.getError()) <= MOE) update = 0; // if withing MOE, keep constant velocity
-    motorSpeed += update;
-    // There's a deadzone where motor speed doesn't do anything but it still takes time to cross ~= +/- 20
-    if (motorSpeed < 20 && motorSpeed > -19) motorSpeed = -20;
-    else if (motorSpeed > -20 && motorSpeed < 19) motorSpeed = 20;
 
-    motorSpeed = constrain(motorSpeed, -PWM_BOUNDS, PWM_BOUNDS);
+
+    // // FSM transition
+    // if (controllerState == 1 && fabs(rollingAvg) > 360 /* °/s */) { // this should be the saturated deg/sec
+    //   controllerState = 0;
+    // } else if (controllerState == 0 && fabs(rollingAvg) < 45 /* °/s */) {
+    //   controllerState = 1;
+    // }
+    // controllerState = 1;
+    
+    // // FSM action 
+    // float update = 0;
+    // if (controllerState == 0) { // state 0 = detumble + update time for angle 
+    //   // update = pidSpeed.compute(0, rollingAvg, timeCur - timePrev); 
+    //   update = pidSpeed.compute(0, rollingAvg, timeCur - timePrev); 
+    //   pidAngle.compute(targetPos, yawAngle, timeCur - timePrev); // need because time is updated each iter
+    // } else { // state 1 = set setpoint to calculated motor output. Error = desired pwm -avg pwm 
+    //   float anglePwmOut = pidAngle.compute(targetPos, yawAngle, timeCur - timePrev);
+    //   update = pidSpeed.compute(anglePwmOut, rollingAvg, timeCur - timePrev);
+    //   #if TEST
+    //     Serial.print("anglePwmOut: "); Serial.print(anglePwmOut);
+    //   #endif
+    // } 
+    // if (fabs(pidAngle.getError()) <= MOE) update = 0; // if withing MOE, keep constant velocity
+    // if (fabs(pidSpeed.getError()) <= MOE) update = 0; // if withing MOE, keep constant velocity
+    // motorSpeed += update;
+    // // There's a deadzone where motor speed doesn't do anything but it still takes time to cross ~= +/- 20
+    // if (motorSpeed < 20 && motorSpeed > -19) motorSpeed = -20;
+    // else if (motorSpeed > -20 && motorSpeed < 19) motorSpeed = 20;
+
+    // motorSpeed = constrain(motorSpeed, -PWM_BOUNDS, PWM_BOUNDS);
     #if TEST
       // Serial.print(" time step: "); Serial.println(timeCur - timePrev);
       // Serial.print(" rollingAvg: "); Serial.print(rollingAvg);
@@ -187,7 +250,7 @@ void loop() {
     #endif
 
     // motorSpeed = pidAngle.compute(targetPos, yawAngle, timeCur - timePrev); // need because time is updated each iter
-    setSpeed(motorSpeed);
+    // setSpeed(motorSpeed);
 
     // printMoreThings();
     #if TEST
